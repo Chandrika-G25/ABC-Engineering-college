570cf9fbbd0349c5790f9d50aaa95fb5e7ae861e
Chandrika-G25 <chandrikag2005.com>
2026-09-08 13:45:18 +0530

    Stage 1: Add core app unit tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('core:home'))
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
